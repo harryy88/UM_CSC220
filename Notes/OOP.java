85f362e48d1088a678b5fa31a321990c89f20269
= <=>
2018-08-30 11:56:02 -0400

    nah

--- Notes/OOP.java
+++ Notes/OOP.java
@@ -1,2 +1,6 @@
-//OOP Lect 2 Notes -- 8/30
+//OOP2 Notes -- 8/30
 
+
+//ArrayList :::::::::::::::::
+//Can only be used with a REF type (String, Objs)
+ArrayList<String> list = new ArrayList<String>(); 
