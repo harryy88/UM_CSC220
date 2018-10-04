6d45095ff51ae42a269ab3db522419d0d36c6134
= <=>
2018-10-03 23:38:51 -0400

    implements a postfix converter to the alg

--- Lab07/src/lab07/Postfixer.java
+++ Lab07/src/lab07/Postfixer.java
@@ -22,8 +22,60 @@ public class Postfixer {
         if (InfixEvaluator("100 * ( 2 + 12 ) / 14") != 100)
             System.err.println("test4 failed --> your answer should have been 100");
         
-        System.out.println("Testing Done!!!");
+       System.out.println("Testing(1) Done!!!");
+        
+
+        if (!PostfixConvertor(new String("(4+5)")).equals("45+"))
+            System.err.println("test1 failed --> should have been 45+");
+        
+        if (!PostfixConvertor(new String("((4+5)*6)")).equals("45+6*"))
+            System.err.println("test2 failed --> should have been 45+6*");
+        
+        if (!PostfixConvertor(new String("((4+((5*6)/7))-8)")).equals("456*7/+8-"))
+            System.err.println("test3 failed --> should have been 456*7/+8-");
+        
+        if (!PostfixConvertor(new String("((4+5*(6-7))/8)")).equals("4567-*+8/"))
+            System.err.println("test4 failed --> should have been 4567-*+8/");
+        
+        if (!PostfixConvertor(new String("(9+(8*7-(6/5^4)*3)*2)")).equals("987*654^/3*-2*+"))
+            System.err.println("test5 failed --> should have been 987*654^/3*-2*+");
+        
+        System.out.println("Testing(2) Done!!!");
+
 	
+	}
+	
+	public static String PostfixConvertor(String line) {
+		
+		StringSplitter data = new StringSplitter(line);
+		Stack<String> operators = new Stack<String>(); 
+		String str = ""; 
+		while ( data.hasNext()) {
+			String token = data.next();
+			
+			if (token.equals("(")) {
+				operators.push(token);
+			}
+			else if (token.equals(")")) {
+				while (!operators.peek().equals("(")) {
+					str = str + operators.pop();
+				}
+				operators.pop(); 
+			}
+			else if (isOperator(token)){
+				while ( !operators.peek().equals("(") && comparePrecedence(operators.peek(), token)) {
+					str += operators.pop(); 
+				}
+				operators.push(token);
+			}
+			else if (!isOperator(token)){  //if is a number 
+				str += token; 
+			}
+			
+			
+		}
+		
+		return str; 
 	}
 	
 	public static double InfixEvaluator(String line) {
@@ -67,9 +119,6 @@ public class Postfixer {
 			operands.push(result);
 		}
 		
-		
-			System.out.println(operands.peek());
-		
 			return operands.pop(); 
 		
 		
@@ -79,16 +128,19 @@ public class Postfixer {
 	public static int tokenPrecedence(String token) {
 		switch ( token ) {
 			case "^": return 4; 
-			case "*": return 3; 
+			case "*": 
+			case "/": return 3;
+			case "-":
 			case "+": return 2; 
 			case "(": return 1; 
 			default: return -1; 
 		}
 	}
 	
-	public static boolean comparePrecedence (String t1, String t2) {
+	public static boolean comparePrecedence(String t1, String t2) {
 		return ( tokenPrecedence(t1) >= tokenPrecedence(t2)); 
 	}
+	
 	
 	public static double evaluate(double num1, double num2, String op) {
 		switch (op) {
